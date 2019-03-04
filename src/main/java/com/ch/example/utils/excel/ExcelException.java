374be8148613f00d3dd5c06943a5c2f1631ce271
chenhao <459984846@qq.com>
2019-03-04 17:28:37 +0800

    easyexcel

---
+++ src/main/java/com/ch/example/utils/excel/ExcelException.java
@@ -0,0 +1,11 @@
+package com.ch.example.utils.excel;
+
+/**
+ * @Author: chenhao
+ * @Date: 2019/3/4 14:00
+ */
+public class ExcelException extends RuntimeException {
+    public ExcelException(String message) {
+        super(message);
+    }
+}
